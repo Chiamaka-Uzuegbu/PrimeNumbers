a34bc122212629d7dac0daf0f86c7bfff7f59971
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2020-11-26 08:58:48 +0100

    Add project files.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Program shows the prime numbers between 1 and 1000");
+            PrintPrimeNumbers();
+            Console.ReadLine();
+        }
+
+        static void PrintPrimeNumbers()
+        {
+            bool primeNumber = true;
+            for (int firstNum = 2; firstNum < 1000; firstNum++)
+            {
+                for (int j = 2; j < 1000; j++)
+                {
+                    if (firstNum % j == 0 && firstNum != j)
+                    {
+                        primeNumber = false;
+                        break;
+                    }
+                }
+                if (primeNumber)
+                {
+                    Console.Write($"\t{firstNum}");
+                }
+                primeNumber = true;
+            }
+        }
+    }
+}
